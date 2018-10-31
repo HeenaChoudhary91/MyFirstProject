e62cc12e85e2cded463468308ae4dd3a62148395
HeenaChoudhary91 <44637552+HeenaChoudhary91@users.noreply.github.com>
2018-10-31 15:43:29 +0530

    Multiplication Added

--- Calculator.java
+++ Calculator.java
@@ -12,10 +12,17 @@ Calculator
        return res;
    }
    
+   int multiply(int x, int y)
+   {
+        int res = x * y;
+        return res;
+   }
+   
   public static void main(String[] args)
   {
      Calculator c = new Calculator();
      c.add(5,5);
      c.subtract(10,5);
+     c.multiply(5,5);
   }
 }
