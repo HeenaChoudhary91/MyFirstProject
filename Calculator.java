c6c2de77fd9b603b870d1b11dc308f258df0bd27
HeenaChoudhary91 <44637552+HeenaChoudhary91@users.noreply.github.com>
2018-10-31 15:38:11 +0530

    Addition & Subtraction Method

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+Calculator
+{
+   int add(int x, int y)
+   {
+       int res = x + y;
+       return res;
+   }
+   
+   int subtract(int x, int y)
+   {
+       int res = x - y;
+       return res;
+   }
+   
+  public static void main(String[] args)
+  {
+     Calculator c = new Calculator();
+     c.add(5,5);
+     c.subtract(10,5);
+  }
+}
